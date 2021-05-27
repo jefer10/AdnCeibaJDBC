29e3757afa58cefde0c93968033c58518ed76314
Jefersson Sebastian Rondon Sanabria - Ceiba Software <jefersson.rondon@ceiba.com.co>
2021-05-26 22:04:43 -0500

    prueba unitaria de la clase servicioActualizarCliente

--- microservicio/dominio/src/test/java/com/ceiba/cliente/servicio/ServicioActualizarClienteTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/cliente/servicio/ServicioActualizarClienteTest.java
@@ -1,4 +1,28 @@
 package com.ceiba.cliente.servicio;
 
+import com.ceiba.BasePrueba;
+import com.ceiba.cliente.modelo.entidad.Cliente;
+import com.ceiba.cliente.puerto.repositorio.RepositorioCliente;
+import com.ceiba.cliente.servicio.testdatabuilder.ClienteTestDataBuilder;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.usuario.puerto.repositorio.RepositorioUsuario;
+import com.ceiba.usuario.servicio.ServicioActualizarUsuario;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class ServicioActualizarClienteTest {
+
+    private static final String EL_CLIENTE_YA_EXISTE_EN_EL_SISTEMA = "El usuario ya existe en el sistema";
+
+    @Test
+    public void validarClienteExistenciaPreviaTest() {
+        // arrange
+        Cliente cliente = new ClienteTestDataBuilder().contId(1l).build();
+        RepositorioCliente repositorioCliente = Mockito.mock(RepositorioCliente.class);
+        Mockito.when(repositorioCliente.existeExcluyendoId(Mockito.anyLong(),Mockito.anyString())).thenReturn(true);
+        ServicioActualizarCliente servicioActualizarCliente = new ServicioActualizarCliente(repositorioCliente);
+        // act - assert
+        BasePrueba.assertThrows(() -> servicioActualizarCliente.ejecutar(cliente), ExcepcionDuplicidad.class,EL_CLIENTE_YA_EXISTE_EN_EL_SISTEMA);
+    }
 }
